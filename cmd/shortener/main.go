99c0651b3201ce8137418a615b08eae2af644d09
Christina Fomenko <kris.inferno@gmail.com>
2022-07-27 18:39:17 +0300

    14-incr

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -57,15 +57,28 @@ func main() {
 	router.Use(compress.Compressing)
 	router.Use(auth.Auth)
 
-	//router.Route("/", func(r chi.Router) {
+	var poolSize int
+	pool := make(chan func(), poolSize)
+	for i := 0; i < poolSize; i++ {
+		go func() {
+			for f := range pool {
+				f()
+			}
+		}()
+	}
+
 	router.Post("/", handlers.New(service, auth, pingSrvc).Shorten)
 	router.Get("/{id}", handlers.New(service, auth, pingSrvc).Expand)
 	router.Post("/api/shorten", handlers.New(service, auth, pingSrvc).APIJSONShorten)
 	router.Get("/api/user/urls", handlers.New(service, auth, pingSrvc).FetchURLs)
 	router.Get("/ping", handlers.New(service, auth, pingSrvc).Ping)
 	router.Post("/api/shorten/batch", handlers.New(service, auth, pingSrvc).ShortenBatch)
-	router.Delete("/api/user/urls", handlers.New(service, auth, pingSrvc).DeleteUserURLs)
-	//})
+
+	go func() {
+		pool <- func() {
+			router.Delete("/api/user/urls", handlers.New(service, auth, pingSrvc).DeleteUserURLs)
+		}
+	}()
 
 	address := cfg.ServerAddress
 	log.WithField("address", address).Info("server starts")
