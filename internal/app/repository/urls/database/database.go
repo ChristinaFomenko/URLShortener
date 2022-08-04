7d36105c9e1782c1f9cbf74fc0eed9775add059a
Christina Fomenko <kris.inferno@gmail.com>
2022-08-04 14:41:35 +0300

    fix

--- internal/app/repository/urls/database/database.go
+++ internal/app/repository/urls/database/database.go
@@ -160,7 +160,7 @@ func (r *pgRepo) DeleteUserURLs(ctx context.Context, toDelete []models.DeleteUse
 
 	defer tx.Rollback()
 
-	stmt, err := tx.PrepareContext(ctx, "UPDATE urls SET deleted = TRUE WHERE user_id = $1 AND url = $2")
+	stmt, err := tx.PrepareContext(ctx, "UPDATE urls SET is_deleted = TRUE WHERE user_id = $1 AND url = $2")
 	if err != nil {
 		return err
 	}
